4e34a6a9719e716eaca21c3d55e5d2d1aa2f0ff1
Piero Silva Morais <piero.morais@gmail.com>
2018-10-27 23:56:48 -0300

    Agora estou testando o comando push para utilizar o github

---
+++ grafos.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*Teste iniciais para utilização do github como plataforma colaborativa*/
+int main(){
+	printf("GIT E GITHUB\n");
+	return 0;
+}
