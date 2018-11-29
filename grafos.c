f3c608a7b24b959a29b28ce703ddc06e18af388e
fourtwenty1 <45470958+fourtwenty1@users.noreply.github.com>
2018-11-29 20:34:39 -0200

    Update grafos.c

--- grafos.c
+++ grafos.c
@@ -1,8 +1,177 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/*Teste iniciais para utilização do github como plataforma colaborativa*/
+#include "Grafo.h"
+
+//Feito por Matheus de Lara Raimundo
+//#vamosnosajudar
+
+
+//Definição do tipo Grafo
+#include <stdio.h>
+struct grafo{
+    int eh_ponderado;
+    int nro_vertices;
+    int grau_max;
+    int** arestas;
+    float** pesos;
+    int* grau;
+};
+
+Grafo* cria_Grafo(int nro_vertices, int grau_max, int eh_ponderado){
+    Grafo *gr;
+    gr = (Grafo*) malloc(sizeof(struct grafo));
+    if(gr != NULL){
+        int i;
+        gr->nro_vertices = nro_vertices;
+        gr->grau_max = grau_max;
+        gr->eh_ponderado = (eh_ponderado != 0)?1:0;
+        gr->grau = (int*) calloc(nro_vertices,sizeof(int));
+
+        gr->arestas = (int**) malloc(nro_vertices * sizeof(int*));
+        for(i=0; i<nro_vertices; i++)
+            gr->arestas[i] = (int*) malloc(grau_max * sizeof(int));
+
+        if(gr->eh_ponderado){
+            gr->pesos = (float**) malloc(nro_vertices * sizeof(float*));
+            for(i=0; i<nro_vertices; i++)
+                gr->pesos[i] = (float*) malloc(grau_max * sizeof(float));
+        }
+
+    }
+    return gr;
+}
+
+void libera_Grafo(Grafo* gr){
+    if(gr != NULL){
+        int i;
+        for(i=0; i<gr->nro_vertices; i++)
+            free(gr->arestas[i]);
+        free(gr->arestas);
+
+        if(gr->eh_ponderado){
+            for(i=0; i<gr->nro_vertices; i++)
+                free(gr->pesos[i]);
+            free(gr->pesos);
+        }
+        free(gr->grau);
+        free(gr);
+    }
+}
+
+int insereAresta(Grafo* gr, int orig, int dest, int eh_digrafo, float peso){
+    if(gr == NULL)
+        return 0;
+    if(orig < 0 || orig >= gr->nro_vertices)
+        return 0;
+    if(dest < 0 || dest >= gr->nro_vertices)
+        return 0;
+
+    gr->arestas[orig][gr->grau[orig]] = dest;
+    if(gr->eh_ponderado)
+        gr->pesos[orig][gr->grau[orig]] = peso;
+    gr->grau[orig]++;
+
+    if(eh_digrafo == 0)
+        insereAresta(gr,dest,orig,1,peso);
+    return 1;
+}
+
+
+void imprime_Grafo(Grafo *gr){
+    if(gr == NULL)
+        return;
+
+    int i, j;
+    for(i=0; i < gr->nro_vertices; i++){
+        printf("%d: ", i);
+        for(j=0; j < gr->grau[i]; j++){
+            if(gr->eh_ponderado)
+                printf("%d(%.2f), ", gr->arestas[i][j], gr->pesos[i][j]);
+            else
+                printf("%d, ", gr->arestas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+int procuraMenorDistancia(float *dist, int *visitado, int NV){
+    int i, menor = -1, primeiro = 1;
+    for(i=0; i < NV; i++){
+        if(dist[i] >= 0 && visitado[i] == 0){
+            if(primeiro){
+                menor = i;
+                primeiro = 0;
+            }else{
+                if(dist[menor] > dist[i])
+                    menor = i;
+            }
+        }
+    }
+    return menor;
+}
+
+void menorCaminho_Grafo(Grafo *gr, int ini, int *ant, float *dist){
+    int i, cont, NV, ind, *visitado, vert;
+    cont = NV = gr->nro_vertices;
+    visitado = (int*) malloc(NV * sizeof(int));
+    for(i=0; i < NV; i++){
+        ant[i] = -1;
+        dist[i] = -1;
+        visitado[i] = 0;
+    }
+    dist[ini] = 0;
+    while(cont > 0){
+        vert = procuraMenorDistancia(dist, visitado, NV);
+        //printf("u = %d\n",u);
+        if(vert == -1)
+            break;
+
+        visitado[vert] = 1;
+        cont--;
+        for(i=0; i<gr->grau[vert]; i++){
+            ind = gr->arestas[vert][i];
+            if(dist[ind] < 0){
+               dist[ind] = dist[vert] + 1;//ou peso da aresta
+               ant[ind] = vert;
+            }else{
+                if(dist[ind] > dist[vert] + 1){
+                    dist[ind] = dist[vert] + 1;//ou peso da aresta
+                    ant[ind] = vert;
+                }
+            }
+        }
+    }
+
+    free(visitado);
+}
+
+
 int main(){
-	printf("GIT E GITHUB\n");
-	return 0;
+    int eh_digrafo = 1;
+    Grafo* gr = cria_Grafo(5, 5, 0);
+	
+	
+    insereAresta(gr, 0, 1, eh_digrafo, 0);
+    insereAresta(gr, 1, 3, eh_digrafo, 0);
+    insereAresta(gr, 1, 2, eh_digrafo, 0);
+    insereAresta(gr, 2, 4, eh_digrafo, 0);
+    insereAresta(gr, 3, 0, eh_digrafo, 0);
+    insereAresta(gr, 3, 4, eh_digrafo, 0);
+    insereAresta(gr, 4, 1, eh_digrafo, 0);
+
+    imprime_Grafo(gr);
+    printf("\nBusca \n");
+    int vis[5];
+   
+
+    int i,ant[5];
+    float dist[5];
+    menorCaminho_Grafo(gr, 0, ant, dist);
+    for(i=0; i<5; i++)
+        printf("%d: %d -> %f\n",i,ant[i],dist[i]);
+
+    libera_Grafo(gr);
+
+    system("pause");
+    return 0;
 }
